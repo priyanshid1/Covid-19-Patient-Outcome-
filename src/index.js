8563b533179be1a19ba2988aa0b0e517482e9f38
Priya Deliwala <priyadeliwala@mac.mynetworksettings.com>
2024-10-27 21:38:49 -0400

    Updated project files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Notice the change to `react-dom/client`
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';  // Optional: Add custom styling if needed
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
