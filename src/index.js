18667169b1a7c90759b278ee61ae9e6193438245
Priya Deliwala <priyadeliwala@mac.mynetworksettings.com>
2024-10-27 21:35:18 -0400

    Update index.js to use createRoot and add deploy script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Notice the change to `react-dom/client`
 import App from './App';
 import './index.css';  // Optional: Add custom styling if needed
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
